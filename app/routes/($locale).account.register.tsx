5a3c7f3522710a983034a003d08f58da21e37e4b
mamala42 <162777991+mamala42@users.noreply.github.com>
2024-03-23 23:44:28 +0200

    Initialize v2.0.1--225e699--aug-02

---
+++ app/routes/($locale).account.register.tsx
@@ -0,0 +1,111 @@
+import {type V2_MetaFunction} from '@remix-run/react';
+import {
+  json,
+  redirect,
+  type ActionFunction,
+  type LoaderArgs,
+} from '@shopify/remix-oxygen';
+
+import {WeaverseContent} from '~/weaverse';
+import {loadWeaversePage} from '~/weaverse/loader.server';
+import {doLogin} from './($locale).account.login';
+
+export async function loader(args: LoaderArgs) {
+  let {context, params} = args;
+  const customerAccessToken = await context.session.get('customerAccessToken');
+
+  if (customerAccessToken) {
+    return redirect(params.locale ? `${params.locale}/account` : '/account');
+  }
+
+  return json({
+    weaverseData: await loadWeaversePage(args),
+  });
+}
+
+type ActionData = {
+  formError?: string;
+};
+
+const badRequest = (data: ActionData) => json(data, {status: 400});
+
+export const action: ActionFunction = async ({request, context, params}) => {
+  const {session, storefront} = context;
+  const formData = await request.formData();
+
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  if (
+    !email ||
+    !password ||
+    typeof email !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return badRequest({
+      formError: 'Please provide both an email and a password.',
+    });
+  }
+
+  try {
+    const data = await storefront.mutate(CUSTOMER_CREATE_MUTATION, {
+      variables: {
+        input: {email, password},
+      },
+    });
+
+    if (!data?.customerCreate?.customer?.id) {
+      /**
+       * Something is wrong with the user's input.
+       */
+      throw new Error(data?.customerCreate?.customerUserErrors.join(', '));
+    }
+
+    const customerAccessToken = await doLogin(context, {email, password});
+    session.set('customerAccessToken', customerAccessToken);
+
+    return redirect(params.locale ? `${params.locale}/account` : '/account', {
+      headers: {
+        'Set-Cookie': await session.commit(),
+      },
+    });
+  } catch (error: any) {
+    if (storefront.isApiError(error)) {
+      return badRequest({
+        formError: 'Something went wrong. Please try again later.',
+      });
+    }
+
+    /**
+     * The user did something wrong, but the raw error from the API is not super friendly.
+     * Let's make one up.
+     */
+    return badRequest({
+      formError:
+        'Sorry. We could not create an account with this email. User might already exist, try to login instead.',
+    });
+  }
+};
+
+export const meta: V2_MetaFunction = () => {
+  return [{title: 'Register'}];
+};
+
+export default function Register() {
+  return <WeaverseContent />;
+}
+
+const CUSTOMER_CREATE_MUTATION = `#graphql
+  mutation customerCreate($input: CustomerCreateInput!) {
+    customerCreate(input: $input) {
+      customer {
+        id
+      }
+      customerUserErrors {
+        code
+        field
+        message
+      }
+    }
+  }
+`;
