a7646a9fae658fe589ac83a661cf28530058bd89
mamala42 <162777991+mamala42@users.noreply.github.com>
2024-03-23 23:44:29 +0200

    Initialize v2.1.0--3491fd5--aug-25

---
+++ app/routes/($locale).account.login.tsx
@@ -0,0 +1,143 @@
+import {type V2_MetaFunction} from '@remix-run/react';
+import {
+  json,
+  redirect,
+  type ActionFunction,
+  type AppLoadContext,
+  type LoaderArgs,
+} from '@shopify/remix-oxygen';
+
+import {WeaverseContent} from '~/weaverse';
+import {loadWeaversePage} from '~/weaverse/loader.server';
+
+export const handle = {
+  isPublic: true,
+};
+
+export async function loader(args: LoaderArgs) {
+  let {context, params} = args;
+  const customerAccessToken = await context.session.get('customerAccessToken');
+
+  if (customerAccessToken) {
+    return redirect(params.locale ? `${params.locale}/account` : '/account');
+  }
+
+  // TODO: Query for this?
+  return json({
+    shopName: 'Hydrogen',
+    weaverseData: await loadWeaversePage(args),
+  });
+}
+
+type ActionData = {
+  formError?: string;
+};
+
+const badRequest = (data: ActionData) => json(data, {status: 400});
+
+export const action: ActionFunction = async ({request, context, params}) => {
+  const formData = await request.formData();
+
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  if (
+    !email ||
+    !password ||
+    typeof email !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return badRequest({
+      formError: 'Please provide both an email and a password.',
+    });
+  }
+
+  const {session, storefront, cart} = context;
+
+  try {
+    const customerAccessToken = await doLogin(context, {email, password});
+    session.set('customerAccessToken', customerAccessToken);
+
+    // Sync customerAccessToken with existing cart
+    const result = await cart.updateBuyerIdentity({customerAccessToken});
+
+    // Update cart id in cookie
+    const headers = cart.setCartId(result.cart.id);
+
+    headers.append('Set-Cookie', await session.commit());
+
+    return redirect(params.locale ? `/${params.locale}/account` : '/account', {
+      headers,
+    });
+  } catch (error: any) {
+    if (storefront.isApiError(error)) {
+      return badRequest({
+        formError: 'Something went wrong. Please try again later.',
+      });
+    }
+
+    /**
+     * The user did something wrong, but the raw error from the API is not super friendly.
+     * Let's make one up.
+     */
+    return badRequest({
+      formError:
+        'Sorry. We did not recognize either your email or password. Please try to sign in again or create a new account.',
+    });
+  }
+};
+
+export const meta: V2_MetaFunction = () => {
+  return [{title: 'Login'}];
+};
+
+export default function Login() {
+  return <WeaverseContent />;
+}
+
+const LOGIN_MUTATION = `#graphql
+  mutation customerAccessTokenCreate($input: CustomerAccessTokenCreateInput!) {
+    customerAccessTokenCreate(input: $input) {
+      customerUserErrors {
+        code
+        field
+        message
+      }
+      customerAccessToken {
+        accessToken
+        expiresAt
+      }
+    }
+  }
+`;
+
+export async function doLogin(
+  {storefront}: AppLoadContext,
+  {
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  },
+) {
+  const data = await storefront.mutate(LOGIN_MUTATION, {
+    variables: {
+      input: {
+        email,
+        password,
+      },
+    },
+  });
+
+  if (data?.customerAccessTokenCreate?.customerAccessToken?.accessToken) {
+    return data.customerAccessTokenCreate.customerAccessToken.accessToken;
+  }
+
+  /**
+   * Something is wrong with the user's input.
+   */
+  throw new Error(
+    data?.customerAccessTokenCreate?.customerUserErrors.join(', '),
+  );
+}
